feeb8edab8b9fc771c70f618f6fbe143ef67bba3
hilaluddinj <hilaluddinj@gmail.com>
2024-09-17 19:47:12 +0700

    add contact section

--- main.js
+++ main.js
@@ -11,3 +11,23 @@ function slideToggle(e) {
   const detail = $(item).children(".about-exp-item-detail");
   $(detail).slideToggle();
 }
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const email = $("#inp-email");
+  const subject = $("#inp-subject");
+  const message = $("#inp-message");
+
+  if (!$(email).val()) {
+    alert("Email is required");
+  } else if (!$(subject).val()) {
+    alert("Subject is required");
+  } else if (!$(message).val()) {
+    alert("Message is Required");
+  } else {
+    alert("Form Submitted");
+    $(email).val("");
+    $(subject).val("");
+    $(message).val("");
+  }
+}
